83cbd867d727ae5f261bb27be5ed7b1d62ab9ed4
adeola <adeolaidowu27@gmail.com>
2020-02-24 18:47:37 +0100

    Setup up app to read from and write to firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,10 +1,18 @@
-import { startAddExpense, addExpense, removeExpense, editExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, removeExpense, editExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk'; 
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
+
+beforeEach((done) => {
+  const expensesData = {};
+  expenses.forEach(({ id, amount, description, note, createdAt }) => {
+  expensesData[id] = { description, amount, note, createdAt }
+});
+  database.ref('expenses').set(expensesData).then(() => done());
+});
 
 test('should set up remove expense action object', () => {
   const action = removeExpense({ id: '1234abc'});
@@ -60,7 +68,7 @@ test('should add expense to database and store', (done) => {
   });
 });
 
-test('should add expense with defaults to database and store', () => {
+test('should add expense with defaults to database and store', (done) => {
   const store = createMockStore({});
   const expenseDefaults = {
     description: "",
@@ -86,16 +94,22 @@ test('should add expense with defaults to database and store', () => {
   });
 });
 
-// test('should set up add expense action generator with default values', () => {
-//   const action = addExpense();
-//   expect(action).toEqual({
-//     type: 'ADD_EXPENSE',
-//     expense: {
-//       description: '',
-//       note: '',
-//       amount: 0,
-//       createdAt: 0,
-//       id: expect.any(String)
-//     }
-//   });
-// });
+test('should set up setExpenses action object', () => {
+  const action = setExpenses(expenses);
+  expect(action).toEqual({
+    type: 'SET_EXPENSES',
+    expenses
+  });
+});
+
+test('should fetch the expense from firebase', (done) => {
+  const store = createMockStore({});
+  store.dispatch(startSetExpenses()).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: 'SET_EXPENSES',
+      expenses
+    });
+    done()
+  });
+});
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -4,5 +4,5 @@ import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({
   adapter: new Adapter()
 });
-jest.setTimeout(30000);
 require('dotenv').config({ path: '.env.test' });
+jest.setTimeout(30000);
